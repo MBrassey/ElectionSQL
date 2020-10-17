4c0a006ce169adba06866a61280ca0907995833d
luc1dLife <luc1d_life@protonmail.com>
2020-10-17 12:00:17 -0600

    Setup Database

--- __tests__/inputCheck.test.js
+++ __tests__/inputCheck.test.js
@@ -1,17 +1,16 @@
-const inputCheck = require('../utils/inputCheck');
+const inputCheck = require("../utils/inputCheck");
 
-test('inputCheck() returns null when all properties exist', () => {
-  const obj = {name: 'alice'};
+test("inputCheck() returns null when all properties exist", () => {
+  const obj = { name: "alice" };
 
-  expect(inputCheck(obj, 'name')).toBe(null);
+  expect(inputCheck(obj, "name")).toBe(null);
 });
 
-test('inputCheck() returns an object when a property is missing', () => {
-  const obj = {name: 'alice', occupation: ''};
-
-  expect(inputCheck(obj, 'name', 'occupation')).toEqual(
+test("inputCheck() returns an object when a property is missing", () => {
+  const obj = { name: "alice", occupation: "" };
+  expect(inputCheck(obj, "name", "occupation")).toEqual(
     expect.objectContaining({
-      error: expect.stringContaining('No occupation specified')
+      error: expect.stringContaining("No occupation specified"),
     })
   );
 });
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
