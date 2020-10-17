2b323d7fbd2623e93ced49cb73e402fde2d352ad
luc1dLife <luc1d_life@protonmail.com>
2020-10-17 14:37:42 -0600

    Start server after db connected

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -7,11 +8,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database("./db/election.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  // Start server after DB connection
+  db.on("open", () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
+  console.log("Connected to the election database.");
+});
+
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
