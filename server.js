70a78a447d9e6273189e48f143eab28b5ef330f7
luc1dLife <luc1d_life@protonmail.com>
2020-10-17 17:43:03 -0600

    Add Candidate

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
-
+const inputCheck = require("./utils/inputCheck");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -70,6 +70,37 @@ app.delete("/api/candidate/:id", (req, res) => {
     res.json({
       message: "successfully deleted",
       changes: this.changes,
+    });
+  });
+});
+
+// Create a candidate
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
     });
   });
 });
