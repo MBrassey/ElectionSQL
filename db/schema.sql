68fb75b429702299e6b6eea53831ebff6d029575
luc1dLife <luc1d_life@protonmail.com>
2020-10-17 22:09:51 -0600

    +Parties Table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ CREATE TABLE candidates (
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
+
+CREATE TABLE parties (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
