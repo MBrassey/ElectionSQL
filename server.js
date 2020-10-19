69e2208d5bd5a83bd04ee5c0cf3fa0c8627cffd6
luc1dLife <luc1d_life@protonmail.com>
2020-10-18 20:43:59 -0600

    Joined Parties

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -41,8 +45,12 @@ app.get("/api/candidates", (req, res) => {
 
 // Get single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
