6d956160e066d0ea381b649b28995110a01e0b85
luc1dLife <luc1d_life@protonmail.com>
2020-10-18 23:31:40 -0600

    Modularized Parties

--- server.js
+++ server.js
@@ -8,62 +8,16 @@ const PORT = process.env.PORT || 3001;
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use("/api", apiRoutes);
 
-// All Parties
-app.get("/api/parties", (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  const params = [];
-  db.all(sql, params, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-// Individual Party
-app.get("/api/party/:id", (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.get(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
-
-// Delete a Party
-app.delete("/api/party/:id", (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.run(sql, params, function (err, result) {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      return;
-    }
-
-    res.json({ message: "successfully deleted", changes: this.changes });
-  });
-});
-
-// Default response for any other requests(Not Found) Catch all
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// start serever after DB connect
+// Start server after DB connection
 db.on("open", () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
